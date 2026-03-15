c5706e1818a6065eb8a0792a5d07fab2ae5cd4f6
K-Rithz <sothearith.kong@student.cadt.edu.kh>
2026-03-15 18:33:40 +0700

    Add Category to ExpenseRecord

---
+++ src/main/java/com/budgettracker/models/transactions/expense/ExpenseCategory.java
@@ -0,0 +1,14 @@
+package com.budgettracker.models.transactions.expense;
+
+public enum ExpenseCategory {
+    FOOD,
+    CLOTHING,
+    TAX,
+    TRANSPORTATION,
+    ENTERTAINMENT,
+    UTILITIES,
+    HEALTHCARE,
+    TECHNOLOGY,
+    EDUCATION,
+    OTHER
+}
