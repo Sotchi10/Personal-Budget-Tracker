2ffa3cfc71a82b23e6378bf51204c0a2aadd2847
Sotchi <sovanpisothyan@example.com>
2026-03-29 22:44:23 +0700

    changes

--- src/main/java/com/budgettracker/models/transactions/ExpenseRecord.java
+++ src/main/java/com/budgettracker/models/transactions/ExpenseRecord.java
@@ -15,6 +15,6 @@ public class ExpenseRecord extends Record {
     @Override
     public String format() {
         // TODO Auto-generated method stub
-        return "Expense  | +$" + getAmount() + " | " + getNote() + " | " + getDate();
+        return "Expense  | -$" + getAmount() + " | " + getNote() + " | " + getDate();
     }
 }
